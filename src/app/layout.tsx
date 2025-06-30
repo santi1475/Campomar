8bd8ecc83980182e7ab5e5d93a29252a75a04d5e
santi1475 <santiguz1475@gmail.com>
2025-06-30 15:47:35 -0500

    Fix: Corregir exportación en layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-export const poppins = Poppins({
+const poppins = Poppins({
   subsets: ["latin"],
   weight: "400",
 });
